13e47e687739a7e6191477ef819d2ff7f1a516b9
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-09-13 13:28:50 +0700

    reponsive header

--- src/components/Header/FullHeader.jsx
+++ src/components/Header/FullHeader.jsx
@@ -1,3 +1,12 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faMagnifyingGlass,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
 import { DropdownButton } from '../Button';
 const navList = [
   {
@@ -7,10 +16,6 @@ const navList = [
   {
     title: 'Xu hướng',
     type: 'hot',
-  },
-  {
-    title: 'Thảo luận',
-    type: 'normal',
   },
   {
     title: 'Tạo bài viết',
@@ -24,43 +29,120 @@ const navList = [
 ];
 
 const FullHeader = () => {
-  return (
-    <header className='w-full sticky top-0 h-16 lg:h-14 bg-primary-bg z-10 shadow-md'>
-      {/* Cotainer */}
-      <div className='mx-[136px]'>
-        {/* Mobile Header*/}
-        {/* Left Header */}
-        <div className='flex items-center'>
-          {/* Logo */}
-          <div className='w-14'>
-            <img src='/logo-blog-rm.png' alt='logo-header' />
-          </div>
+  const [isOpenMenu, setOpenMenu] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    console.log(isOpenMenu);
+  }, [isOpenMenu]);
 
-          {/* Navigation */}
-          <div className='flex'>
-            {navList.map((item, index) =>
-              item.type === 'submenu' ? (
-                <DropdownButton key={index} title={item.title} submenu={item.children} />
-              ) : (
-                <button
-                  key={index}
-                  className='p-4 text-white text-sm font-semibold uppercase hover:bg-gray-bg hover:text-gray-font duration-300'
-                >
-                  {item.title}
-                  {item.type === 'hot' ? (
-                    <span className='px-2.5 py-0.5 ml-3 bg-orange-bg-btn rounded-full '>HOT</span>
-                  ) : (
-                    <span></span>
-                  )}
-                </button>
-              )
-            )}
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 1024 && isOpenMenu) {
+      setOpenMenu(false);
+    }
+  }, [size.width, isOpenMenu]);
+
+  return (
+    <div>
+      <header
+        className={`w-full ${
+          isOpenMenu ? 'hidden' : 'sticky'
+        } top-0 h-16 lg:h-14 bg-primary-bg z-10 shadow-md`}
+      >
+        {/* Container */}
+        <div className="lg:mx-32 flex justify-between">
+          {/* Left Header */}
+          <div className="flex items-center">
+            {/* Logo */}
+            <div className="w-16 lg:w-14 cursor-pointer">
+              <img src="/logo-blog-rm.png" alt="logo-header" />
+            </div>
+            <span className="lg:hidden text-2xl text-white font-bold tracking-wider">
+              TechEBlog.
+            </span>
+            {/* Navigation */}
+            <div className="hidden lg:flex">
+              {navList.map((item, index) =>
+                item.type === 'submenu' ? (
+                  <DropdownButton
+                    key={index}
+                    title={item.title}
+                    submenu={item.children}
+                  />
+                ) : (
+                  <button
+                    key={index}
+                    className="p-3.5 text-white text-sm font-semibold uppercase hover:bg-gray-bg hover:text-gray-font duration-300"
+                  >
+                    {item.title}
+                    {item.type === 'hot' ? (
+                      <span className="px-2.5 py-0.5 ml-3 bg-orange-bg-btn rounded-full ">
+                        HOT
+                      </span>
+                    ) : (
+                      <></>
+                    )}
+                  </button>
+                )
+              )}
+            </div>
+          </div>
+          {/* Right Header */}
+          <div className="flex items-center gap-x-3">
+            <Link
+              to="/trang-tim-kiem"
+              className="flex item-center justify-center w-10 h-10 rounded-full text-xl text-white hover:bg-white hover:text-gray-font duration-300"
+            >
+              <span className="my-auto">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </span>
+            </Link>
+
+            <button className="uppercase p-2.5 lg:px-3 lg:py-1.5 bg-white rounded-full text-sm font-semibold">
+              <span className="hidden lg:block">Đăng nhập</span>
+              <FontAwesomeIcon
+                icon={faUser}
+                className="block lg:hidden w-5 h-5"
+              />
+            </button>
+
+            <button
+              className="lg:hidden mr-3 p-2 rounded-full hover:bg-white text-white hover:text-gray-font duration-200"
+              onClick={() => setOpenMenu(true)}
+            >
+              <FontAwesomeIcon icon={faBars} className="block w-6 h-6" />
+            </button>
           </div>
         </div>
+      </header>
+      {/* Mobile Header*/}
 
-        {/* Right Header */}
+      <div
+        className={`bg-blue-300 z-30 w-screen h-screen ${
+          isOpenMenu
+            ? 'animate-switchLeft'
+            : 'transition translate-x-full duration-300 -z-10'
+        }`}
+      >
+        <button className="" onClick={() => setOpenMenu(false)}>
+          X
+        </button>
       </div>
-    </header>
+    </div>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,44 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                /*===== LOGO =====*/
-                'yellow-logo': '#f9f03b',
-                'green-logo': '#00feb2',
-                'blue-logo': '#1982ff',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        /*===== LOGO =====*/
+        'yellow-logo': '#f9f03b',
+        'green-logo': '#00feb2',
+        'blue-logo': '#1982ff',
 
-                /*===== FONT, BUTTON & BORDER =====*/
-                'gray-font': '#292929',
-                'light-gray-font': '#7A7A7A',
-                'gray-submenu-font': '#555555',
-                'green-font': '#428F5E',
-                'gray-border': '#757575',
-                'gray-bg-btn': '#F2F3F5',
-                'green-bg-btn': '#12D8B2',
-                'orange-bg-btn': '#e27602',
-                'light-blue-bg': '#E8F0FE',
+        /*===== FONT, BUTTON & BORDER =====*/
+        'gray-font': '#292929',
+        'light-gray-font': '#7A7A7A',
+        'gray-submenu-font': '#555555',
+        'green-font': '#428F5E',
+        'gray-border': '#757575',
+        'gray-bg-btn': '#F2F3F5',
+        'green-bg-btn': '#12D8B2',
+        'orange-bg-btn': '#e27602',
+        'light-blue-bg': '#E8F0FE',
 
-                /*===== HEADER =====*/
-                'primary-bg': '#292929',
-                'gray-bg': '#F2F3F5',
-                'dark-gray-bg': '#BFBFBF',
+        /*===== HEADER =====*/
+        'primary-bg': '#292929',
+        'gray-bg': '#F2F3F5',
+        'dark-gray-bg': '#BFBFBF',
 
-                /*===== ERROR =====*/
-                'error-font' : '#B72727',
-                'error-bg': '#FFF8F8',
-            },
-            animation: {
-              fade: 'fadeOut 5s ease-in-out',
-            },
-            keyframes: theme => ({
-              fadeOut: {
-                '0%': { backgroundColor: theme('colors.red.300') },
-                '100%': { backgroundColor: theme('colors.transparent') },
-              },
-            }),
+        /*===== ERROR =====*/
+        'error-font': '#B72727',
+        'error-bg': '#FFF8F8',
+      },
+      animation: {
+        switchLeft: 'rightToLeft 0.3s ease-in-out',
+      },
+      keyframes: {
+        rightToLeft: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
         },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
